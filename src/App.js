4f1d52059285bc1f7264a30500f8ad54571c4822
DRT <daniriian@gmail.com>
2018-07-18 11:32:27 +0300

    add event handler for markers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import Map from './components/Map';
 import './App.css';
 
+const places = [
+  { name: "Babes Park", position: { lat: 46.764567, lng: 23.561235 }, id: "1" },
+  { name: "Central Park", position: { lat: 46.7691526, lng: 23.5783746 }, id: "2" },
+  { name: "Cetatuia Park", position: { lat: 46.7753634, lng: 23.581478 }, id: "3" },
+  { name: "Tetarom Park", position: { lat: 46.777249, lng: 23.559200 }, id: "4" },
+  { name: "Cluj Arena", position: { lat: 46.768784, lng: 23.572644 }, id: "5" },
+  { name: "CFR Arena", position: { lat: 46.779516, lng: 23.577538 }, id: "6" }
+]
+
+
 class App extends Component {
+
+  onMarkerClick = () => {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map locations={places} onMarkerClick={this.onMarkerClick} />
       </div>
     );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -9,22 +9,37 @@ class Map extends Component {
         loadMapJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyDfXtdZ3qGQ0hVQkPdf2mU1mEYl3hvhpOs&callback=initMap')
     }
 
-    initMap() {
+    initMap = () => {
         const mapview = document.getElementById('map');
-
+        //instantiate the map
         const map = new window.google.maps.Map(mapview, {
             center: { lat: 46.7667271, lng: 23.5823639 },
             zoom: 14,
             mapTypeControl: false
+        });
+
+        //Create markers
+        let markers = this.props.locations.map(location => {
+            new window.google.maps.Marker({
+                position: location.position,
+                map: map,
+                animation: window.google.maps.Animation.DROP,
+                title: location.name
+            })
+        });
+
+        //Event handler for markers (click)
+        markers.forEach(marker => {
+            marker.addListener("click", () => {
+                this.props.onMarkerClick(marker);
+            });
         });
     }
 
 
     render() {
         return (
-            <div id="map">
-
-            </div>
+            <div id="map"></div>
         );
     }
 }
